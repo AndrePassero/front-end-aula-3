a41ac0930bb57281b76da00822fd6079fc823029
André Passero <111911506+AndrePassero@users.noreply.github.com>
2022-08-30 12:34:50 -0300

    Add files via upload

---
+++ script3_01.js
@@ -0,0 +1,10 @@
+let oct = 010;
+let bit = 0b10, hexa = 0x10;
+let dia = 25;
+dia = 25;
+nota = 9.7;
+document.write (dia);
+document.write("<br>");
+document.write (nota);
+document.write("<br>");
+document.write(oct,"<br>",bin,"<br>",hexa);
